0daa601916d5bd6af965458dd79c4741d0c11d05
Edge <edge27452696803@gmail.com>
2024-08-03 17:53:43 +0800

    mvc

--- src/main/java/main/example/controller/userController.java
+++ src/main/java/main/example/controller/userController.java
@@ -21,7 +21,7 @@ public class userController
         return "save";
     }
 
-    @RequestMapping
+    @DeleteMapping
     public String delete(@PathVariable Integer id)
     {
         System.out.println("delete " + id);
