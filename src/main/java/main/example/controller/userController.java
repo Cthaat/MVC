7c5a64447de2bd4bd4f4504d2a3be494c07d231e
Edge <edge27452696803@gmail.com>
2024-08-03 17:28:55 +0800

    mvc

--- src/main/java/main/example/controller/userController.java
+++ src/main/java/main/example/controller/userController.java
@@ -45,7 +45,7 @@ public class userController
     @ResponseBody
     public String select(@PathVariable Integer id)
     {
-        System.out.println("put");
+        System.out.println("get ");
         return "put";
     }
 }
