9951b7eb5a00ea82ba966843fd64e39cf720e0cb
Edge <edge27452696803@gmail.com>
2024-08-03 13:34:11 +0800

    mvc

--- src/main/java/main/example/config/springConfig.java
+++ src/main/java/main/example/config/springConfig.java
@@ -1,5 +1,8 @@
 package main.example.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @Description: spring
  * @ClassName: springConfig
@@ -7,6 +10,9 @@ package main.example.config;
  * @Date: 2024/8/3 13:30
  * @Version: 1.0
  */
+
+@Configuration
+@ComponentScan ({"main.example.service" , "main.example.dao"})
 public class springConfig
 {
 }
