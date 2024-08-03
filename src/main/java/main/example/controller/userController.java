b6469d696bdf06e7622242fd4973375d6df6c72c
Edge <edge27452696803@gmail.com>
2024-08-03 17:50:09 +0800

    mvc

--- src/main/java/main/example/controller/userController.java
+++ src/main/java/main/example/controller/userController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping ("/users")
+@ResponseBody
 public class userController
 {
     @RequestMapping (method = RequestMethod.POST)
