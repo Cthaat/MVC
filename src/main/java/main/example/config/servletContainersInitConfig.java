a13ee6666c2c1acd4a1d59153fe2757fbd0c2329
Edge <edge27452696803@gmail.com>
2024-08-03 13:34:42 +0800

    mvc

--- src/main/java/main/example/config/servletContainersInitConfig.java
+++ src/main/java/main/example/config/servletContainersInitConfig.java
@@ -2,7 +2,6 @@ package main.example.config;
 
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
 /**
